6aa5a1a0974acc267950727d32b35b5649ef0c99
Evan Carroll <me@evancarroll.com>
2016-11-02 13:25:04 -0500

    tabs fail

---
+++ trec.sql
@@ -0,0 +1,38 @@
+CREATE TEMP TABLE broker_sales (
+  license_type varchar(4),
+  license_number text,
+  full_name text,
+  suffix text,
+  license_status smallint,
+  original_license text,
+  license_expiration text,
+  education_status smallint,
+  mce_status smallint,
+  designated_supervisor_flag text,
+  phone_number text,
+  email_address text,
+  mailing_address_line_1 text,
+  mailing_address_line_2 text,
+  mailing_address_line_3 text,
+  mailing_address_city text,
+  mailing_address_state_code text,
+  mailing_address_zip_code text,
+  mailing_address_county_code text,
+  physical_address_line_1 text,
+  physical_address_line_2 text,
+  physical_address_line_3 text,
+  physical_address_city text,
+  physical_address_state_code text,
+  physical_address_zip_code text,
+  physical_address_county_code text,
+  related_license_type text,
+  related_license_number text,
+  related_license_full_name text,
+  related_license_suffix text,
+  relationship_start_date text,
+  agency_identifier text,
+  full_name_last_first text
+);
+
+\COPY broker_sales FROM 'trecfile.csv' WITH ( FORMAT CSV, DELIMITER E'\t', HEADER );
+
